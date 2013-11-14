51636ac14054a40b0cc107362920d5e8fef66557
Jonathan R. Wallace <jonathan.wallace@gmail.com>
2013-11-14 12:07:17 -0500

    adds default controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 HelloWorld::Application.routes.draw do
+  get "hello/world"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
